700391449dc4067b7922b8746d90224fb0a64979
adityakhare18 <khareaditya0105@gmail.com>
2025-04-06 22:34:03 +0530

    Fix MongoDB connection to use await for proper async handling

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-    mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('MongoDB connected');
     })
