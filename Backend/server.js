fa0e8e04e120e375e832076311d38899ad4ea248
adityakhare18 <khareaditya0105@gmail.com>
2025-04-09 15:15:28 +0530

    Add Cloudinary configuration and product/user models to backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import connectCloudinary from './config/cloudinary.js';
 connectDB()
+connectCloudinary();
 
 //App config
 const app = express()
